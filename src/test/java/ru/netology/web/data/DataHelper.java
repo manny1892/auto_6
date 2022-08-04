fd215681207decd79996585859f0a4cd1aa214bc
manny1892 <manny1892@gmail.com>
2022-08-04 20:10:38 +0300

    hw commit#fix

--- src/test/java/ru/netology/web/data/DataHelper.java
+++ src/test/java/ru/netology/web/data/DataHelper.java
@@ -3,43 +3,44 @@ package ru.netology.web.data;
 import lombok.Value;
 
 public class DataHelper {
-  private DataHelper() {}
+    private DataHelper() {
+    }
 
-  @Value
-  public static class AuthInfo {
-    private String login;
-    private String password;
-  }
+    @Value
+    public static class AuthInfo {
+        private String login;
+        private String password;
+    }
 
-  public static AuthInfo getAuthInfo() {
-    return new AuthInfo("vasya", "qwerty123");
-  }
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
 
-  public static AuthInfo getOtherAuthInfo(AuthInfo original) {
-    return new AuthInfo("petya", "123qwerty");
-  }
+    public static AuthInfo getOtherAuthInfo(AuthInfo original) {
+        return new AuthInfo("petya", "123qwerty");
+    }
 
-  @Value
-  public static class VerificationCode {
-    private String code;
-  }
+    @Value
+    public static class VerificationCode {
+        private String code;
+    }
 
-  public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
-    return new VerificationCode("12345");
-  }
+    public static VerificationCode getVerificationCodeFor(AuthInfo authInfo) {
+        return new VerificationCode("12345");
+    }
 
-  @Value
-  public static class CardData {
-    private String number;
-  }
+    @Value
+    public static class CardData {
+        private String number;
+    }
 
-  public static CardData getFirstCardNumber() {
-    return new CardData("5559 0000 0000 0001");
-  }
+    public static CardData getFirstCardNumber() {
+        return new CardData("5559 0000 0000 0001");
+    }
 
-  public static CardData getSecondCardNumber() {
-    return new CardData("5559 0000 0000 0002");
-  }
+    public static CardData getSecondCardNumber() {
+        return new CardData("5559 0000 0000 0002");
+    }
 
 
 }
--- src/test/java/ru/netology/web/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/web/test/MoneyTransferTest.java
@@ -1,4 +1,5 @@
 package ru.netology.web.test;
+
 import lombok.val;
 
 import org.junit.jupiter.api.Assertions;
@@ -11,54 +12,54 @@ import ru.netology.web.page.LoginPageV2;
 import static com.codeborne.selenide.Selenide.open;
 
 class MoneyTransferTest {
-@BeforeEach
-  void shouldLogin(){
-    open("http://localhost:9999");
-    var loginPage = new LoginPageV2();
-    var authInfo = DataHelper.getAuthInfo();
-    var verificationPage = loginPage.validLogin(authInfo);
-    var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
-    verificationPage.validVerify(verificationCode);
-}
+    @BeforeEach
+    void shouldLogin() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPageV2();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
 
 
-  @Test
-  void shouldTransferMoneySecondToFirstCard() {
-    int value = 100;
-    val dashboardPage = new DashboardPage();
-    var FirstCardInitialBalance = dashboardPage.getFirstCardBalance();
-    var SecondCardInitialBalance = dashboardPage.getSecondCardBalance();
-    val dashboardPage2 = dashboardPage.TransferMoneySecondToFirst(value);
-    var FirstCardFinalBalance = dashboardPage2.getFirstCardBalance();
-    var SecondCardFinalBalance = dashboardPage2.getSecondCardBalance();
-    Assertions.assertEquals(SecondCardInitialBalance - value, SecondCardFinalBalance);
-    Assertions.assertEquals(FirstCardInitialBalance + value, FirstCardFinalBalance);
+    @Test
+    void shouldTransferMoneySecondToFirstCard() {
+        int value = 100;
+        val dashboardPage = new DashboardPage();
+        var FirstCardInitialBalance = dashboardPage.getFirstCardBalance();
+        var SecondCardInitialBalance = dashboardPage.getSecondCardBalance();
+        val dashboardPage2 = dashboardPage.TransferMoneySecondToFirst(value);
+        var FirstCardFinalBalance = dashboardPage2.getFirstCardBalance();
+        var SecondCardFinalBalance = dashboardPage2.getSecondCardBalance();
+        Assertions.assertEquals(SecondCardInitialBalance - value, SecondCardFinalBalance);
+        Assertions.assertEquals(FirstCardInitialBalance + value, FirstCardFinalBalance);
 
-  }
+    }
 
-  @Test
-  void shouldTransferMoneyFirstToSecondCard() {
-    int value = 100;
-    val dashboardPage = new DashboardPage();
-    var FirstCardInitialBalance = dashboardPage.getFirstCardBalance();
-    var SecondCardInitialBalance = dashboardPage.getSecondCardBalance();
-    val dashboardPage2 = dashboardPage.TransferMoneyFirstToSecond(value);
-    var FirstCardFinalBalance = dashboardPage2.getFirstCardBalance();
-    var SecondCardFinalBalance = dashboardPage2.getSecondCardBalance();
-    Assertions.assertEquals(FirstCardInitialBalance - value, FirstCardFinalBalance);
-    Assertions.assertEquals(SecondCardInitialBalance + value, SecondCardFinalBalance);
+    @Test
+    void shouldTransferMoneyFirstToSecondCard() {
+        int value = 100;
+        val dashboardPage = new DashboardPage();
+        var FirstCardInitialBalance = dashboardPage.getFirstCardBalance();
+        var SecondCardInitialBalance = dashboardPage.getSecondCardBalance();
+        val dashboardPage2 = dashboardPage.TransferMoneyFirstToSecond(value);
+        var FirstCardFinalBalance = dashboardPage2.getFirstCardBalance();
+        var SecondCardFinalBalance = dashboardPage2.getSecondCardBalance();
+        Assertions.assertEquals(FirstCardInitialBalance - value, FirstCardFinalBalance);
+        Assertions.assertEquals(SecondCardInitialBalance + value, SecondCardFinalBalance);
 
-  }
+    }
 
-  @Test
-  void DoNotShouldTransferMoneyFirstToSecondCardAfterLimit() {
-    int value = 100;
-    val dashboardPage = new DashboardPage();
-    var SecondCardInitialBalance = dashboardPage.getSecondCardBalance();
-    val dashboardPage2 = dashboardPage.TransferMoneySecondToFirst(value+SecondCardInitialBalance);
-    dashboardPage2.getNotification();
+    @Test
+    void DoNotShouldTransferMoneyFirstToSecondCardAfterLimit() {
+        int value = 100;
+        val dashboardPage = new DashboardPage();
+        var SecondCardInitialBalance = dashboardPage.getSecondCardBalance();
+        val dashboardPage2 = dashboardPage.TransferMoneySecondToFirst(value + SecondCardInitialBalance);
+        dashboardPage2.getNotification();
 
-  }
+    }
 
 }
 
